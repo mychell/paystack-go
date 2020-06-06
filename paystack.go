2c6588ab646e284dc3be08df2bbd7c11a4a12f87
Michael <adeyerimichael@gmail.com>
2020-06-06 22:33:18 +0100

    Update paystack.go

--- paystack.go
+++ paystack.go
@@ -21,7 +21,7 @@ const (
 	version = "0.1.0"
 
 	// defaultHTTPTimeout is the default timeout on the http client
-	defaultHTTPTimeout = 24 * time.Second
+	defaultHTTPTimeout = 20 * time.Second
 
 	// base URL for all Paystack API requests
 	baseURL = "https://api.paystack.co"
